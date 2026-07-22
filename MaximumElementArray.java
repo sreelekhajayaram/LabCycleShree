7c115fd1abc924b9635f93bdb18708694dd94e26
sreelekhajayaram <sreelekhaajayaram@gmail.com>
2026-07-22 12:35:42 +0530

    Add files via upload

---
+++ MaximumElementArray.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class MaximumElementArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+
+        int[] array = new int[n];
+
+        System.out.println("Enter the array elements:");
+        for (int i = 0; i < n; i++) {
+            array[i] = sc.nextInt();
+        }
+
+        int max = array[0];
+
+        for (int i = 1; i < n; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+
+        System.out.println("Largest element = " + max);
+
+        sc.close();
+    }
+}
